5c1b386e97b1501124cfe9c8f3ada3c5f97dd2b8
EduardoMoraesRitter <eduardo.moraes.ritter@gmail.com>
2016-05-04 17:26:22 -0300

    npm

--- Aula/aula6-npm.js
+++ Aula/aula6-npm.js
@@ -10,14 +10,36 @@ site: https://www.npmjs.com/
 
 Abaixo vou lançar 3 comandos para atualizarmos nosso node.
 
-npm cache clean -f
-npm install -g n
-n stable
+npm cache clean -f    --limpar o modulos
+npm install -g n      --instalar novamente
+n stable               --
 
 E para finalizar
 node --version
 
+--node-gyp is required to build RobotJS.
+--Install node-gyp using npm
+npm install -g node-gyp
+
+--configurar variavel
+npm config set python C:\Python27\python.exe
+
+
 npm install imagemin-pngquant
+
+sequencia AI
+--intalar pyton versao antiga, configurar variavel
+npm config set python C:\Python27\python.exe
+npm install -g node-gyp
+npm install --save robotjs
+npm install --save synaptic
+npm install --save async
+npm install --save lodash
+npm install --save blessed-contrib
+npm install --save blessed
+
+https://www.npmjs.com/package/robotjs
+https://stackoverflow.com/questions/21365714/nodejs-error-installing-with-npm/21366601#21366601?newreg=e5cb74b781a74c5289d5bdd51a4c61c4
 
 */
 
