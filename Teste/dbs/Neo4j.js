c109e8ecc88f56524114e38ea8bcae6e0fb5f307
Eduardo Ritter <eduardo.moraes.ritter@gmail.com>
2016-09-18 23:29:20 -0300

    neo4j

--- Teste/dbs/Neo4j.js
+++ Teste/dbs/Neo4j.js
@@ -7,3 +7,22 @@ https://github.com/tbaum/heroku-node-neo4js
 
 http://nodejsconfit.levelgraph.io/
  */
+
+var neo4j = require('neo4j');
+var db = new neo4j.GraphDatabase('http://username:password@localhost:7474');
+
+db.cypher({
+    query: 'MATCH (u:User {email: {email}}) RETURN u',
+    params: {
+        email: 'alice@example.com',
+    },
+}, function(err, results) {
+    if (err) throw err;
+    var result = results[0];
+    if (!result) {
+        console.log('No user found.');
+    } else {
+        var user = result['u'];
+        console.log(JSON.stringify(user, null, 4));
+    }
+});
